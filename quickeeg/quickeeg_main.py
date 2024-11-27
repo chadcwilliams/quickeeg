d91903d421fb1b7194bc7225c20f5868eb302363
chadcwilliams <cwilliams.uca@gmail.com>
2024-11-27 10:59:29 -0800

    Changed some params

--- quickeeg/quickeeg_main.py
+++ quickeeg/quickeeg_main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     ###########################################
 
     #Subject information
-    path = 'quickeeg/data'
+    path = os.path.join('quickeeg','data')
     id = 'A_011_FU1_x'
 
     #Create pipeline
@@ -27,11 +27,14 @@ if __name__ == '__main__':
                       '21': [f'{i}' for i in range(21, 30)],
                       '31': [f'{i}' for i in range(31, 40)]}
     
+    #temp:
+    target_markers = {'11': '11', '21': '21', '31': '31'}
+    
     params = {'pipeline':               pipeline,
               'file_path':              os.path.join(path, id),
               'find_files_by_marker':   's11',
               'reference_channels':     'average',
-              'bp_filter_cutoffs':      [0.1, 8],
+              'bp_filter_cutoffs':      [0.1, 50],
               'notch_filter_freq':      60,
               'ica_components':         20,
               'eog_channel':            ['1L', '1R'],
@@ -42,5 +45,5 @@ if __name__ == '__main__':
     #Run the pipeline
     preprocessing = Preprocessing(**params)
     preprocessing.process()
-    preprocessing.plot_erp(electrode_index=2)
+    preprocessing.plot_erp(electrode_index=2, save_plot=True)
     print('Debug stop')
